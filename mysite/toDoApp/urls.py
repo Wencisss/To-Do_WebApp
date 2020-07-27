b2637f05610fcd5d5d155b06355f492fee514ccb
Wencisss <st43190@student.upce.cz>
2020-07-27 16:10:32 +0200

    Improvement of the aplication

--- mysite/toDoApp/urls.py
+++ mysite/toDoApp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 from . import views
 
-app_name = 'toDoApp'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('todo', views.todo, name='todo')
 ]
--- mysite/toDoApp/views.py
+++ mysite/toDoApp/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
+def todo(request):
+    template = loader.get_template('toDoApp/todo.html')
+    return HttpResponse(template.render())
+
 def home(request):
     template = loader.get_template('toDoApp/home.html')
     return HttpResponse(template.render())
